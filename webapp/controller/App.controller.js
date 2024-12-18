0e33f5b0ccada84c91adfeaad21dc98e6e11d829
Shashank <shashank5114001@gmail.com>
2024-12-18 14:21:50 +0530

    basic UI structure covered

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,12 +7,12 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("ui5.learnui5init.controller.App", {
-        onShowHello() {
-            // read msg from i18n model
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg", [sRecipient,"Shank"] );
-            MessageToast.show(sMsg);
-        }
+        // onShowHello() {
+        //     // read msg from i18n model
+        //     const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        //     const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        //     const sMsg = oBundle.getText("helloMsg", [sRecipient,"Shank"] );
+        //     MessageToast.show(sMsg);
+        // }
     });
 });
